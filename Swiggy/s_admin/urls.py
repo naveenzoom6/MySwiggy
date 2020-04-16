be3af623bab5f82451a06966b76db02d67228ad7
shubhamj96 <Shubhamjoshi9921@gmail.com>
2020-04-16 19:48:14 +0530

    16/4/2020 commit
    This commit of upto Crud Operetion of State, city. area. type.(completing the task)

--- Swiggy/s_admin/urls.py
+++ Swiggy/s_admin/urls.py
@@ -34,19 +34,21 @@ urlpatterns = [
     #City
     path('open_city/', views.open_city, name="open_city"),
     path('save_city/', views.save_city, name="save_city"),
-    # path('update_city/', views.update_city, name="update_city"),
-    # path('update_city_data/', views.update_city_data, name="update_city_data"),
-    # path('delete_city/', views.delete_city, name='delete_city'),
+    path('update_city/', views.update_city, name="update_city"),
+    path('update_city_data/', views.update_city_data, name="update_city_data"),
+    path('delete_city/', views.delete_city, name='delete_city'),
 
     #Area
     path('open_area/', views.open_area, name="open_area"),
     path('save_area/', views.save_area, name="save_area"),
-    # path('update_area/', views.update_area, name="update_area"),
-    # path('update_area_data/', views.update_area_data, name="update_area_data"),
-    # path('delete_area/', views.delete_area, name='delete_area'),
+    path('update_area/', views.update_area, name="update_area"),
+    path('update_area_data/', views.update_area_data, name="update_area_data"),
+    path('delete_area/', views.delete_area, name='delete_area'),
 
     #Type
     path('open_type/', views.open_type, name="open_type"),
     path('save_type/', views.save_type, name="save_type"),
-
+    path('update_type/', views.update_type, name="update_type"),
+    path('update_type_data/', views.update_type_data, name="update_type_data"),
+    path('delete_type/', views.delete_type, name='delete_type'),
 ]
