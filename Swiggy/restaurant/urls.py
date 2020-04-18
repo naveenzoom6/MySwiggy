ee33dfb82dc4be6479a2a6d6736baf5feeaf5fac
AndroidNaveen <naveenandroid6557@gmail.com>
2020-04-18 21:15:48 +0530

    Final Day

--- Swiggy/restaurant/urls.py
+++ Swiggy/restaurant/urls.py
@@ -23,4 +23,8 @@ urlpatterns = [
     path('',views.showMain,name="restro"),
     path('register/',views.registerPage,name="register"),
     path('save_res/',views.save_res,name="save_res"),
+    path('restro_login/',views.restro_login,name="restro_login"),
+    path('restro_login_check/',views.restro_login_check,name="restro_login_check"),
+    path('restro_home/',views.restro_home,name="restro_home"),
+
 ]
